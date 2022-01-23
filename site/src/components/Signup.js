47b2bb0aa9f6d692231ba2f01afade8a3f181ef7
paula hage <hage.paula@hotmail.com>
2022-01-23 20:10:15 +0100

    #3 Form creation

--- site/src/components/Signup.js
+++ site/src/components/Signup.js
@@ -1,15 +1,48 @@
 import React, { useState } from "react";
+import TextField from "@mui/material/TextField";
+import Box from "@mui/material/Box";
+import Button from "@mui/material/Button";
 
 const Signup = () => {
   const [name, setName] = useState("");
   const [email, setEmail] = useState("");
   const [password, setPassword] = useState("");
 
+  const handleSubmit = async () => {
+    const response = await fetch("http://localhost:3001/users", {
+      method: "POST",
+      headers: {
+        body: { name, email, password }
+      },
+    });
+    const responseCode = response.status;
+    console.log(responseCode);
+  };
+
   return (
-    <div>
-      <label htmlFor="">Name: </label>
-      <input className="valid-input"></input>
-    </div>
+    <Box>
+      <div>
+        <TextField
+          label="Name"
+          onChange={(e) => setName(e.target.value)}
+        />
+      </div>
+      <div>
+        <TextField
+          label="Email"
+          onChange={(e) => setEmail(e.target.value)}
+        />
+      </div>
+      <div>
+        <TextField
+          label="Password"
+          onChange={(e) => setPassword(e.target.value)}
+        />
+      </div>
+      <Button variant="contained" onClick={handleSubmit}>
+        Submit
+      </Button>
+    </Box>
   );
 };
 
