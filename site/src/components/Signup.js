4e57dcefb3f1c055fe1bfebf3895aebcf4551cb5
paula hage <hage.paula@hotmail.com>
2022-01-23 23:48:25 +0100

    #3 Creation of  dynamic validation

--- site/src/components/Signup.js
+++ site/src/components/Signup.js
@@ -1,59 +1,104 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 import TextField from "@mui/material/TextField";
 import Box from "@mui/material/Box";
 import Button from "@mui/material/Button";
-import * as yup from "yup"
+import * as yup from "yup";
 
 
 const Signup = () => {
+
+  // States for the form fields
   const [name, setName] = useState("");
   const [email, setEmail] = useState("");
   const [password, setPassword] = useState("");
-  const [errorName, setErrorName] = useState("")
+
+  // States for validation error messages
+  const [errorName, setErrorName] = useState("");
   const [errorEmail, setErrorEmail] = useState("");
   const [errorPassword, setErrorPassword] = useState("");
 
+  // State to watch the first render
+  const [firstRender, setFirstRender] = useState(true);
+
+
+
+  // Effects to watch each typing in the inputs fields to fire error messages
+  useEffect(() => {
+    if (firstRender) {
+      setFirstRender(false);
+    } else {
+      nameValidate();
+    }
+  }, [name]);
+
+  useEffect(() => {
+    if (firstRender) {
+      setFirstRender(false);
+    } else {
+      emailValidate();
+    }
+  }, [email]);
+
+  useEffect(() => {
+    if (firstRender) {
+      setFirstRender(false);
+    } else {
+      passwordValidate();
+    }
+  }, [password]);
+
+
+
+  // Setting the validation inputs with Yup
   const nameValidation = yup
-      .string()
-      .required("Name is required")
-      .min(2, "Name has to at least 2 characters")
+    .string()
+    .required("Name is required")
+    .min(2, "Name has to at least 2 characters");
 
   const emailValidation = yup
-      .string()
-      .required("Email is required")
-      .email("Invalid email address")
+    .string()
+    .required("Email is required")
+    .email("Invalid email address");
 
   const passwordValidation = yup
-      .string()
-      .min(6, "Minimum of 6 characters")
+    .string()
+    .min(6, "Minimum of 6 characters");
 
 
 
-  const handleSubmit = async () => {
+  // Validating the inputs
+  const nameValidate = async() => {
     try {
-      await nameValidation.validate(name)
-      setErrorName("")
+      await nameValidation.validate(name);
+      setErrorName("");
     } catch (error) {
       setErrorName(error.errors[0]);
 
       console.log(JSON.stringify(error, undefined, 2));
-
     }
+  };
 
-    try {
-      await emailValidation.validate(email)
-      setErrorEmail("")
-    } catch (error) {
-      setErrorEmail(error.errors[0]);
-    }
+  const emailValidate = async() => {
+     try {
+       await emailValidation.validate(email);
+       setErrorEmail("");
+     } catch (error) {
+       setErrorEmail(error.errors[0]);
+     }
+  };
 
+  const passwordValidate = async() => {
     try {
       await passwordValidation.validate(password);
       setErrorPassword("");
     } catch (error) {
-      setErrorPassword(error.errors[0])
+      setErrorPassword(error.errors[0]);
     }
+  };
 
+
+  //Send to API the data of the user after sign up
+  const handleSubmit = async () => {
 
     // const response = await fetch("http://localhost:3001/users", {
     //   method: "POST",
@@ -63,12 +108,7 @@ const Signup = () => {
     // });
     // const responseCode = response.status;
     // console.log(responseCode);
-
-
-
   };
-
-
 
   return (
     <Box>
