2a4d6b8fab053ff472db4be7b8b5ee6a18289f19
paula hage <hage.paula@hotmail.com>
2022-01-31 22:11:47 +0100

    #3 complementation of  the form validation

--- site/src/components/Signup.js
+++ site/src/components/Signup.js
@@ -4,9 +4,7 @@ import Box from "@mui/material/Box";
 import Button from "@mui/material/Button";
 import * as yup from "yup";
 
-
 const Signup = () => {
-
   // States for the form fields
   const [name, setName] = useState("");
   const [email, setEmail] = useState("");
@@ -20,15 +18,14 @@ const Signup = () => {
   // State to watch the first render
   const [firstRender, setFirstRender] = useState(true);
 
-
-
   // Effects to watch each typing in the inputs fields to fire error messages
   useEffect(() => {
     if (firstRender) {
       setFirstRender(false);
-    } else {
-      nameValidate();
+      return;
     }
+
+    nameValidate();
   }, [name]);
 
   useEffect(() => {
@@ -47,8 +44,6 @@ const Signup = () => {
     }
   }, [password]);
 
-
-
   // Setting the validation inputs with Yup
   const nameValidation = yup
     .string()
@@ -62,32 +57,29 @@ const Signup = () => {
 
   const passwordValidation = yup
     .string()
+    .required("Password is required")
     .min(6, "Minimum of 6 characters");
 
-
-
   // Validating the inputs
-  const nameValidate = async() => {
+  const nameValidate = async () => {
     try {
       await nameValidation.validate(name);
       setErrorName("");
     } catch (error) {
       setErrorName(error.errors[0]);
-
-      console.log(JSON.stringify(error, undefined, 2));
     }
   };
 
-  const emailValidate = async() => {
-     try {
-       await emailValidation.validate(email);
-       setErrorEmail("");
-     } catch (error) {
-       setErrorEmail(error.errors[0]);
-     }
+  const emailValidate = async () => {
+    try {
+      await emailValidation.validate(email);
+      setErrorEmail("");
+    } catch (error) {
+      setErrorEmail(error.errors[0]);
+    }
   };
 
-  const passwordValidate = async() => {
+  const passwordValidate = async () => {
     try {
       await passwordValidation.validate(password);
       setErrorPassword("");
@@ -96,18 +88,26 @@ const Signup = () => {
     }
   };
 
-
   //Send to API the data of the user after sign up
   const handleSubmit = async () => {
+    nameValidate();
+    emailValidate();
+    passwordValidate();
 
-    // const response = await fetch("http://localhost:3001/users", {
-    //   method: "POST",
-    //   headers: {
-    //     body: { name, email, password }
-    //   },
-    // });
-    // const responseCode = response.status;
-    // console.log(responseCode);
+    const response = await fetch("http://localhost:3001/users", {
+      method: "POST",
+      headers: {
+        body: { name, email, password },
+      },
+    });
+    //const responseCode = response.status;
+    setFirstRender(true);
+    setName("");
+    setFirstRender(true);
+    setEmail("");
+    setFirstRender(true);
+    setPassword("");
+
   };
 
   return (
@@ -115,6 +115,7 @@ const Signup = () => {
       <div>
         <TextField
           label="Name"
+          value={name}
           onChange={(e) => setName(e.target.value)}
           error={errorName !== ""}
           helperText={errorName}
@@ -123,6 +124,7 @@ const Signup = () => {
       <div>
         <TextField
           label="Email"
+          value={email}
           onChange={(e) => setEmail(e.target.value)}
           error={errorEmail !== ""}
           helperText={errorEmail}
@@ -131,6 +133,7 @@ const Signup = () => {
       <div>
         <TextField
           label="Password"
+          value={password}
           onChange={(e) => setPassword(e.target.value)}
           error={errorPassword !== ""}
           helperText={errorPassword}
